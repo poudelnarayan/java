a6faccc5cd8fed86bb90eef686c9b4b8782cd3ef
Narayan Poudel <poudelnarayan434@gmail.com>
2022-06-11 12:47:07 +0545

    inheritance

--- OOP/classesAndObjects/src/App.java
+++ OOP/classesAndObjects/src/App.java
@@ -3,23 +3,21 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-// Car Class
-        Car  prosche = new Car();
+        // Car Class
+        Car prosche = new Car();
         prosche.setModels("911");
         System.out.println("Model is " + prosche.getModels());
-// Account Class
-        Account bobsAccount = new Account("12333" ,"Bob Brown", 0.00 , "bob@gmail.com" , "(+98) 456-4346");
+        // Account Class
+        Account bobsAccount = new Account("12333", "Bob Brown", 0.00, "bob@gmail.com", "(+98) 456-4346");
 
         System.out.println("Enter the amount to deposit");
         double depositAmount = scanner.nextDouble();
         scanner.nextLine();
 
-
         bobsAccount.deposit(depositAmount);
         bobsAccount.withDraw(100000);
 
         VipCustomer person1 = new VipCustomer();
-
 
     }
 }
---
+++ OOP/inheritance/src/Dog.java
@@ -0,0 +1,40 @@
+public class Dog extends Animal{
+
+    private int eyes;
+    private int legs;
+    private int tail;
+    private int teeth;
+    private String coat;
+
+
+    public Dog(String name,  int size, int weight , int eyes , int legs , int tail  , int teeth , String coat) {
+        super(name, 1, 1, size, weight);
+        this.eyes = eyes;
+        this.legs = legs;
+        this.tail = tail;
+        this.teeth = teeth;
+        this.coat = coat;
+    }
+
+    private void chew(){
+        System.out.println("Dog chewing..");
+    }
+
+    public void walk() {
+        System.out.println("Dog walking");
+        move(2);
+    }
+
+    public  void run () {
+        System.out.println("Dog running");
+        move(10);
+    }
+
+
+    @Override
+    public void eat() {
+        System.out.println("Dog eating...");
+        chew();
+        super.eat();
+    }
+}
