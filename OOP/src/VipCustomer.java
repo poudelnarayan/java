ebd1ce10075564a79e9d764a60f1d7b431183026
Narayan Poudel <poudelnarayan434@gmail.com>
2022-06-11 10:25:05 +0545

    oop

--- OOP/src/VipCustomer.java
+++ OOP/src/VipCustomer.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class VipCustomer {
+public class VipCustomer {
+    private String name;
+    private double creditLimit;
+    private  String emailAddress;
+
+    public VipCustomer(){
+        this("Default Name" , 0.0 , "Unknown email");
+    }
+
+    public VipCustomer(String name, String emailAddress) {
+        this(name, 0.0 , emailAddress);
+
+    }
+
+    public VipCustomer(String name, double creditLimit, String emailAddress) {
+        this.name = name;
+        this.creditLimit = creditLimit;
+        this.emailAddress = emailAddress;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCreditLimit() {
+        return creditLimit;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
 }
